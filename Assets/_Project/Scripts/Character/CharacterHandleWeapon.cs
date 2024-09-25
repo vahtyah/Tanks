59c86a979432c18fef6204967fa6de05482eaa4a
Tyah <8268826@gmail.com>
2024-09-26 01:15:31 +0300

    Clean folder, update particle

---
+++ Assets/_Project/Scripts/Character/CharacterHandleWeapon.cs
@@ -0,0 +1,39 @@
+﻿using MoreMountains.Feedbacks;
+using UnityEngine;
+
+public class CharacterHandleWeapon : CharacterAbility
+{
+    //Binding
+    [SerializeField] private Transform weaponHolder;
+    [SerializeField] private Transform projectileSpawnPoint;
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private MMF_Player weaponUseFeedback;
+    
+    [SerializeField] private Weapon weapon;
+
+    protected override void Initialization()
+    {
+        Pool.Register(projectilePrefab);
+        weapon = Instantiate(weapon, weaponHolder);
+        weapon.Owner = character;
+        weapon.SetProjectileSpawnTransform(projectileSpawnPoint);
+        base.Initialization();
+        //NOTE: Clear
+    }
+
+    public override void ProcessAbility()
+    {
+        base.ProcessAbility();
+        HandleInput();
+    }
+
+    protected override void HandleInput()
+    {
+        base.HandleInput(); 
+        if (characterInput.GetFireButton())
+        {
+            weapon.WeaponUse();
+            weaponUseFeedback?.PlayFeedbacks();
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/UI/MainMenu/GUIMainMenuManger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIMainMenuManger : MonoBehaviour
+{
+    [SerializeField] private Button twoPlayersButton;
+    [SerializeField] private Button threePlayersButton;
+    [SerializeField] private Button fourPlayersButton;
+    
+    
+    private void Start()
+    {
+        twoPlayersButton.onClick.AddListener(() => ButtonSelectAmountPlayerOnClick(2));
+        threePlayersButton.onClick.AddListener(() => ButtonSelectAmountPlayerOnClick(3));
+        fourPlayersButton.onClick.AddListener(() => ButtonSelectAmountPlayerOnClick(4));
+    }
+
+    private void ButtonSelectAmountPlayerOnClick(int value)
+    {
+        Scene.Load("MultiDisplay");
+    }
+}
